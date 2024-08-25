e3d84fce4262a997128420173a8c1d434d010b13
jonnef <kennethbolland@online.de>
2024-08-25 22:48:17 +0200

    initial commit

---
+++ src/app/kanban/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewKanbanItemPage = () => {
+  return (
+    <div>
+      <h1>Create a new Kanban Item</h1>
+    </div>
+  )
+}
+
+export default NewKanbanItemPage
---
+++ src/app/kanban/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeButton from '../components/buttons/Home'
+
+const KanbanPage = () => {
+  return (
+    <div>
+      <h1>Kanban board</h1>
+      <HomeButton />
+    </div>
+  )
+}
+
+export default KanbanPage
